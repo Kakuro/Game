9ca6455534b7d2a493beb33bb043eabfbb4e14d8
andrej <andrej@andrej-PC>
2013-01-10 18:44:40 +0100

    add test

---
+++ Kakuro/src/test/scala/de/htwg/kakuro/model/PlayFieldSpec.scala
@@ -0,0 +1,11 @@
+package de.htwg.kakuro.model
+
+import org.specs2.mutable._
+
+
+class PlayFieldSpec extends SpecificationWithJUnit {
+
+	"A new PlayField with defalut field" should {
+		val field  = new PlayField
+	}
+}
